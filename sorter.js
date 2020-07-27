c6ed3134f925be2c29c54d7e31d0b5c9fc2de540
Kajal Chingsubam <kchings1@binghamton.edu>
2020-07-27 00:03:00 -0400

    skeleton code using sort() method

---
+++ sorter.js
@@ -0,0 +1,27 @@
+var numElement = document.getElementById("numbers");
+var numBoxElement = document.querySelector("#num-box");
+var enteredNumbers = document.querySelector(".numbersEntered");
+var sortButton = document.querySelector("#sortButton");
+var afterSort = document.querySelector(".afterSort");
+let numList = [];
+
+numBoxElement.addEventListener("keypress", check);
+
+function check(event) {
+  if (event.keyCode == 13) {
+    var num = numBoxElement.value;
+    numList.push(num);
+    console.log(numList);
+    numBoxElement.value = "";
+
+    enteredNumbers.innerHTML = numList;
+  }
+}
+
+sortButton.addEventListener("click", function () {
+  numList.sort(function (a, b) {
+    return a - b;
+  });
+
+  afterSort.innerHTML = numList;
+});
