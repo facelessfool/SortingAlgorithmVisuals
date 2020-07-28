6b45b783dc9a6c85af73f2e8bbe9fe2a6c95d4e9
Kajal Chingsubam <kchings1@binghamton.edu>
2020-07-28 02:39:48 -0400

    implemented bubble sort

--- sorter.js
+++ sorter.js
@@ -3,6 +3,7 @@ var numBoxElement = document.querySelector("#num-box");
 var enteredNumbers = document.querySelector(".numbersEntered");
 var sortButton = document.querySelector("#sortButton");
 var afterSort = document.querySelector(".afterSort");
+var BubbleBtn = document.querySelector("#Bubble");
 let numList = [];
 
 numBoxElement.addEventListener("keypress", check);
@@ -28,6 +29,23 @@ sortButton.addEventListener("click", function () {
   numList.sort(function (a, b) {
     return a - b;
   });
+
+  afterSort.innerHTML = numList;
+});
+
+BubbleBtn.addEventListener("click", function () {
+  var swapped;
+  do {
+    swapped = false;
+    for (var i = 0; i < numList.length - 1; i++) {
+      if (numList[i] > numList[i + 1]) {
+        var temp = numList[i];
+        numList[i] = numList[i + 1];
+        numList[i + 1] = temp;
+        swapped = true;
+      }
+    }
+  } while (swapped);
 
   afterSort.innerHTML = numList;
 });
